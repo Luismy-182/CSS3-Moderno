545d6935356a36b7e8cb17c982a217ee3378d6c8
Miguel Angel Suarez <miguel.buny@gmail.com>
2026-07-08 15:58:51 -0600

    gulp ahora puede minificar imagenes

--- 11-cafetería/gulpfile.js
+++ 11-cafetería/gulpfile.js
@@ -1,21 +1,60 @@
-const { src, dest, watch, series, parallel } = require('gulp');
+import gulp from 'gulp';
+const { src, dest, watch, series } = gulp;
+//tools for images
+// Herramientas para imágenes
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+import avif from 'gulp-avif';
 
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
 
-const css = (done) => {
-    //compilando sass
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+const sass = gulpSass(dartSass); //
+
+export const css = (done) => {
     src('src/scss/app.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(postcss([autoprefixer()]))
-        .pipe(dest('build/css'))
+        .pipe(dest('build/css'));
+    done();
+};
+
+//imagenes ligeras con gulp
+
+const imagenes= (done)=>{
+    const opciones={
+        optimizationLevel:3
+    }
+    src('src/img/**/*')
+    .pipe(imagemin({opciones}))
+    .pipe(dest('build/img'));
     done();
 }
-const dev = () => {
-    watch('src/scss/**/*.scss', css);
+
+const versionWebp=()=>{
+    return src('src/img/**/*.{png,jpg}')
+    .pipe(webp())
+    .pipe(dest('build/img'))
 }
 
-exports.css=css;
-exports.dev=dev;
-exports.default=series(css, dev);
+const versionAvif=(done)=>{
+    const opciones={
+        quality:50
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
+
+
+
+//ahora esucha por imagenes y por scss
+const dev = () => {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+export default series(imagenes, versionWebp, versionAvif, css, dev);
