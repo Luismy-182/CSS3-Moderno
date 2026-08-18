fb6f859dc9025cdbef678e68ec1785a5acc1b5e5
Miguel Angel Suarez <miguel.buny@gmail.com>
2026-08-18 12:08:05 -0600

    carolina spa terminado

---
+++ 16-carolinaspa/build/js/app.js
@@ -0,0 +1,36 @@
+const swipper = () => {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        autoplay: {
+            delay: 4000
+        },
+        effect: 'cube',
+        cubeEffect: {
+            slideShadows: true,
+            shadow: true,
+            shadowOffset: 20
+        },
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', swipper);
